f6081950b1d21f2f05b0b20caea6308ef286729e
John Mark <wanjugujohnmark@gmail.com>
2023-08-15 16:52:43 +0300

    initial commit

---
+++ How To Make Weather App Using JavaScript Step By Step Explained - YouTube_files/blade 1.php
@@ -0,0 +1,101 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left mb-2">
+                    <h2>Add New Employee</h2>
+                </div>
+                <div class="d-box justify-content-end">
+                    <a class="btn btn-primary" href="{{ route('employees.index') }}"> Back</a>
+                </div>
+            </div>
+        </div>
+        @if(session('status'))
+        <div class="alert alert-success mb-1 mt-1">
+            {{ session('status') }}
+        </div>
+        @endif
+        <form action="{{ route('employees.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Photo Upload</strong>
+                        <input type="file" name="photo" class="form-control"accept=".jpg,.jpeg, .png," required>
+                        @error('photo')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Employee Name:</strong>
+                        <input type="text" name="name" class="form-control" placeholder="Employee Name">
+                        @error('name')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Employee Phone Number:</strong>
+                        <input type="text" name="phone_number" class="form-control" placeholder="Phone Number">
+                        @error('phone_number')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Employee Email:</strong>
+                        <input type="email" name="email" class="form-control" placeholder="Email">
+                        @error('email')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Employee Address:</strong>
+                        <input type="text" name="address" class="form-control" placeholder="Address">
+                        @error('name')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Department:</strong>
+                        <input type="text" name="department" class="form-control" placeholder="department">
+                        @error('department')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Salary:</strong>
+                        <input type="number" name="salary" class="form-control" placeholder="Salary">
+                        @error('salary')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Hiring Date:</strong>
+                        <input type="text" name="hiring_date" class="form-control" placeholder="YY-MM-DD">
+                        @error('hiring_date')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+            </div>
+        </form>
+    </div>
+@endsection
+
